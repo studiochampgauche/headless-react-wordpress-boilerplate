75c5552027d64cb248eba1b30652a63e9d3adf40
studiochampgauche <comptes@champgauche.studio>
2023-11-30 09:50:42 -0500

    Update Core Plugin Description

--- src/extensions/cg-core/template/core.php
+++ src/extensions/cg-core/template/core.php
@@ -4,7 +4,7 @@ Plugin Name: Champ Gauche Core
 Author: Studio Champ Gauche
 Author URI: https://champgauche.studio
 Plugin URI: https://wpboilerplate.champgauche.studio
-Description: Manage WordPress and add-ons of Champ Gauche.
+Description: A plugin that handles repetitive needs in each project and our add-ons.
 Requires at least: 6.4.1
 Requires PHP: 8.2
 Version: 3.0.0
