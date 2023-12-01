c594cc1df6cab5880542df6e96112559bec03285
studiochampgauche <comptes@champgauche.studio>
2023-12-01 03:36:49 -0500

    REST API and Editor now done in Core plugin

--- src/themes/the-theme/template/functions.php
+++ src/themes/the-theme/template/functions.php
@@ -4,23 +4,6 @@
 
 		function __construct(){
 
-			/*
-			* Protect REST API
-			*/
-			add_filter('rest_authentication_errors', function($result) {
-
-				if (!current_user_can('manage_options')){
-					return new WP_Error(
-						'cannot_access_rest',
-						__('JSON REST API IS PROTECTED'),
-						['status' => 403]
-					);
-				}
-
-				return $result;
-
-			});
-
 
 			/*
 			* Preload
@@ -32,7 +15,8 @@
 				echo '<link rel="preload" as="image" href="">';
 				
 			}, 3);
-			*/
+            */
+			
 
 			/*
 			* Enqueue Scripts
@@ -40,24 +24,10 @@
 			add_action('wp_enqueue_scripts', function(){
 
 				wp_localize_script('scg-main', 'SYSTEM', [
-					'ajaxurl' => admin_url('admin-ajax.php'),
-					'lang' => 'fr'
+					'ajaxurl' => admin_url('admin-ajax.php')
 				]);
 				
 			}, 11);
-
-
-			/*
-			* admin_init
-			*/
-			add_action('admin_init', function(){
-
-				/*
-				* Remove Editor on page
-				*/
-				remove_post_type_support('page', 'editor');
-
-			});
 
 		}
 
