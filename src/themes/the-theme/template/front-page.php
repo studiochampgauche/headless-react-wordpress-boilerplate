cf1cf915ad9d44361a296ee3c94c04985fadad99
studiochampgauche <comptes@champgauche.studio>
2023-12-01 03:43:38 -0500

    Remove old codes

--- src/themes/the-theme/template/front-page.php
+++ src/themes/the-theme/template/front-page.php
@@ -7,20 +7,5 @@ get_header();
 while(have_posts()) : the_post();
 
 
-	$coming_path = scg::tp('coming-soon.php');
-	$front_path = scg::tp('front-page.php');
-
-	$include = $front_path;
-
-
-	$user = wp_get_current_user();
-	$roleArray = $user->roles;
-	$userRole = isset($roleArray[0]) ? $roleArray[0] : '';
-	
-	if(scg::field('maintenance') === 'enable' && !in_array($userRole, ['administrator']))
-		$include = $coming_path;
-
-
-	include $include;
 
 endwhile; get_footer(); ?>
