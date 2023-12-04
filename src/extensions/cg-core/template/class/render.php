287f6a897b144b4ba16c9fdd8e082059b7aeb0a2
studiochampgauche <comptes@champgauche.studio>
2023-12-04 11:17:54 -0500

    Add Render

---
+++ src/extensions/cg-core/template/class/render.php
@@ -0,0 +1,31 @@
+<?php
+    
+namespace StudioChampGauche\Render;
+
+class Render{
+    
+    public static $wp_heads = [];
+    
+    function __construct(){
+        
+        add_action('wp_head', function(){
+            
+			if(self::$wp_heads)
+            	echo implode('', self::$wp_heads);
+			
+        }, 1);
+        
+    }
+    
+    
+    public static function addToHeadTag($value){
+
+        self::$wp_heads[] = $value;
+
+    }
+    
+}
+
+new Render();
+
+?>
