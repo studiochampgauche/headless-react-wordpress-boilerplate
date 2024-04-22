5397baacb68762a709a598bb06a5a028846d2d39
Studio Champ Gauche <comptes@champgauche.studio>
2024-04-21 21:07:46 -0400

    Change " for '

--- src/themes/the-theme/js/PageScroller.js
+++ src/themes/the-theme/js/PageScroller.js
@@ -1,7 +1,7 @@
 'use strict';
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ScrollSmoother } from 'gsap/ScrollSmoother';
 
 
 class PageScroller{
