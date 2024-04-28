7db8b7febb92aed7d958733d9abb046c1e61357f
Studio Champ Gauche <comptes@champgauche.studio>
2024-04-27 22:35:37 -0400

    Allow image import

--- src/built/webpack.config.js
+++ src/built/webpack.config.js
@@ -28,6 +28,18 @@ const main = {
 					'sass-loader'
 				],
 			},
+			{
+				test: /\.(png|jpg|gif|svg)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: '../images/'
+						}
+					}
+				]
+			}
 		],
 	},
 	plugins: [
