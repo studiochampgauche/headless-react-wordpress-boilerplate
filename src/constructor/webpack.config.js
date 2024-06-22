10de1ca682ddb2d4c0a4983a96f2653b6b7aaa4d
Studio Champ Gauche <comptes@champgauche.studio>
2024-06-22 19:09:36 -0400

    Remove Gulp/Update Webpack and increase his power

--- src/constructor/webpack.config.js
+++ src/constructor/webpack.config.js
@@ -2,15 +2,18 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import CopyPlugin from 'copy-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import ImageMinimizerPlugin from 'image-minimizer-webpack-plugin';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const main = {
-	entry: [
-		'../themes/the-theme/js/App.js',
-		'../themes/the-theme/scss/App.scss'
-	],
+	entry: {
+		app: [
+			'../themes/the-theme/js/App.js',
+			'../themes/the-theme/scss/App.scss'
+		]
+	},
 	output: {
 		filename: 'main.min.js',
 		path: path.resolve(__dirname, '../../wp-content/themes/the-theme/assets/js')
@@ -22,7 +25,10 @@ const main = {
 				use: [
 					{
 						loader: 'file-loader',
-						options: {outputPath: '../css/', name: 'main.min.css'}
+						options: {
+							name: 'main.min.css',
+							outputPath: '../css/'
+						}
 					},
 					'sass-loader'
 				],
@@ -34,7 +40,7 @@ const main = {
 						loader: 'file-loader',
 						options: {
 							name: '[name].[ext]',
-							outputPath: '../images/'
+							outputPath: '../images/',
 						}
 					}
 				]
@@ -55,6 +61,16 @@ const main = {
 					noErrorOnMissing: true
 				},
 				{
+					from: '../themes/the-theme/images',
+					to: path.resolve(__dirname, '../../wp-content/themes/the-theme/assets/images'),
+					noErrorOnMissing: true
+				},
+				{
+					from: '../themes/the-theme/fonts',
+					to: path.resolve(__dirname, '../../wp-content/themes/the-theme/assets/fonts'),
+					noErrorOnMissing: true
+				},
+				{
 					from: '../extensions/cg-core/template',
 					to: path.resolve(__dirname, '../../wp-content/plugins/cg-core'),
 					noErrorOnMissing: true
@@ -65,8 +81,27 @@ const main = {
 					noErrorOnMissing: true
 				}
 			]
-		})
+		}),
 	],
+	optimization: {
+		minimizer: [
+			new ImageMinimizerPlugin({
+				minimizer: {
+					implementation: ImageMinimizerPlugin.imageminMinify,
+					options: {
+						plugins: [
+							['gifsicle', { interlaced: true }],
+							['jpegtran', { progressive: true }],
+							['mozjpeg', { quality: 75 }],
+							['optipng', { optimizationLevel: 5 }],
+							['pngquant', { quality: [0.65, 0.90], speed: 4, }],
+							['svgo', { plugins: [{ name: 'preset-default', params: { overrides: { removeViewBox: false }}}]}]
+						],
+					},
+				},
+			})
+		],
+	},
 	resolve: {
 		modules: [
 			path.resolve(__dirname, 'node_modules')
