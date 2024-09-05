e12218801ddfcc9f72618f42c24065b2f1a1fcc9
Studio Champ Gauche <comptes@champgauche.studio>
2024-09-05 09:28:55 -0400

    Clean up

--- src/back/theme/footer.php
+++ src/back/theme/footer.php
@@ -1,4 +1,3 @@
 
-<?php wp_footer(); ?>
 </body>
 </html>
--- src/back/theme/header.php
+++ src/back/theme/header.php
@@ -1,8 +1,5 @@
 <?php ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
-	<?php wp_head(); ?>
-</head>
-
-<body data-base-url="<?= home_url(); ?>" data-barba="wrapper">
+<head></head>
+<body>
