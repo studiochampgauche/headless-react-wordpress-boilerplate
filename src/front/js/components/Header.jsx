9f54cc887caec5a14652a0124d9851917089d81f
Studio Champ Gauche <comptes@champgauche.studio>
2024-10-13 23:15:17 -0400

    Remove Logo and Nav

--- src/front/js/components/Header.jsx
+++ src/front/js/components/Header.jsx
@@ -1,8 +1,6 @@
 'use strict';
 import React from 'react';
 import { Link } from 'react-router-dom';
-import MainNav from './Nav.jsx';
-import MainLogo from './Logo.jsx';
 
 const Header = () => {
 	
@@ -10,10 +8,7 @@ const Header = () => {
 		<header>
 			<div className="plywood">
 				<div className="container">
-					<Link className="logo" to="/" data-transition="true">
-						<MainLogo alt="Demo" />
-					</Link>
-					<MainNav />
+					
 				</div>
 			</div>
 		</header>
